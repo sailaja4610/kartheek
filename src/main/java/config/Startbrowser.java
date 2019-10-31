324fdb4388e31cc996fad808eca613382b9b5942
User <User@DESKTOP-0M0A5K6>
2019-10-31 17:42:07 +0530

    HELLO

---
+++ src/main/java/config/Startbrowser.java
@@ -0,0 +1,21 @@
+package config;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Startbrowser {
+	
+  @Test
+  public void f() {
+	 // WebDriver driver=new FirefoxDriver();
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
